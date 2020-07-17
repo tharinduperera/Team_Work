770993f694fdd93c7bb876658ea97e6364d7b78d
SanoP <sanodeemantha@gmail.com>
2020-07-17 22:21:06 +0530

    Crate UI of ViewToDo

--- src/controller/ViewTodoListController.java
+++ src/controller/ViewTodoListController.java
@@ -1,4 +1,25 @@
 package controller;
 
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextArea;
+
 public class ViewTodoListController {
+    public JFXButton btnNewToDO;
+    public ListView txtToDoList;
+    public JFXButton btnToaDayList;
+    public TextArea txtDescription;
+    public JFXButton btnDelete;
+    public Label lblDate;
+
+    public void btnNewToDOOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnToaDayListOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnDeleteOnAction(ActionEvent actionEvent) {
+    }
 }
