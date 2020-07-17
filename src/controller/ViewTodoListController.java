efbf000c50ab70f1942864483f10b7df4dc6391a
SanoP <sanodeemantha@gmail.com>
2020-07-17 23:56:33 +0530

    Set inserted titles to list

--- src/controller/ViewTodoListController.java
+++ src/controller/ViewTodoListController.java
@@ -1,6 +1,8 @@
 package controller;
 
+import DBConnection.DBConnection;
 import com.jfoenix.controls.JFXButton;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -13,15 +15,43 @@ import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class ViewTodoListController {
     public JFXButton btnNewToDO;
-    public ListView txtToDoList;
+    public ListView<String> txtToDoList;
     public JFXButton btnToaDayList;
     public TextArea txtDescription;
     public JFXButton btnDelete;
     public Label lblDate;
     public AnchorPane root;
+
+    public void initialize(){
+        loadToDoList();
+    }
+
+    private void loadToDoList() {
+        Connection connection = DBConnection.getInstance().getConnection();
+
+        try {
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("select title from todolist");
+            ObservableList<String> items = txtToDoList.getItems();
+            items.clear();
+
+            while (resultSet.next()){
+                String string = resultSet.getString(1);
+                items.add(string);
+            }
+
+            txtToDoList.refresh();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
 
     public void btnNewToDOOnAction(ActionEvent actionEvent) throws IOException {
         Scene scene = new Scene(FXMLLoader.load(this.getClass().getResource("/view/CreateNewToDo.fxml")));
